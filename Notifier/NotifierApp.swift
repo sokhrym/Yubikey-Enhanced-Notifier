3aa47198d081a11775d99aebd0b51ca77c32a81d
Stanislav Okhrym <sokhrym@duosecurity.com>
2025-03-25 20:35:02 +0200

    Initial Commit

---
+++ Notifier/NotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifierApp.swift
+//  Notifier
+//
+//  Created by sokhrym on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotifierTests/NotifierTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotifierTests.swift
+//  NotifierTests
+//
+//  Created by sokhrym on 25.03.2025.
+//
+
+import Testing
+@testable import Notifier
+
+struct NotifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
